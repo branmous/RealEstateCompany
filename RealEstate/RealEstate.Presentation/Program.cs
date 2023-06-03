76e064e3b611e387d4c039b0617f608b90407726
branmous <brandon-377@hotmail.com>
2023-06-03 09:46:33 -0500

    Inject dependency

--- RealEstate/RealEstate.Presentation/Program.cs
+++ RealEstate/RealEstate.Presentation/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
+using RealEstate.Domain.Interfaces;
 using RealEstate.Infrastructure.Data;
+using RealEstate.Infrastructure.Repositories;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,7 +13,12 @@ builder.Services.AddControllers()
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=LocalConnection", b=> b.MigrationsAssembly("RealEstate.Presentation")));
+builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=LocalConnection", b => b.MigrationsAssembly("RealEstate.Presentation")));
+builder.Services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
+builder.Services.AddScoped<IPropertyRepository, PropertyRepository>();
+builder.Services.AddScoped<IPropertyImageRepository, PropertyImageRepository>();
+builder.Services.AddScoped<IPropertyTraceRepository, PropertyTraceRepository>();
+builder.Services.AddScoped<IOwnerRepository, OwnerRepository>();
 
 var app = builder.Build();
 
