e2a796d596e3bd86e660ce2f49973e34ebeb00d7
branmous <brandon-377@hotmail.com>
2023-06-06 01:05:28 -0500

    init test controller

---
+++ RealEstate/RealEstate.Presentation/Controllers/PropertyImagesController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RealEstate.Domain.Exceptions;
+using RealEstate.Domain.Interfaces.Services;
+using System.Net;
+
+namespace RealEstate.Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PropertyImagesController : ControllerBase
+    {
+        private readonly IPropertyService _propertyService;
+        private readonly IPropertyImageService _propertyImageService;
+
+        public PropertyImagesController(IPropertyService propertyService,
+            IPropertyImageService propertyImageService)
+        {
+            this._propertyService = propertyService;
+            _propertyImageService = propertyImageService;
+        }
+
+        [HttpPost("{id:int}/SetImages")]
+        public async Task<IActionResult> PostSetImagesAsync(int id, List<IFormFile> propertyImages)
+        {
+            try
+            {
+                if (propertyImages.Count == 0)
+                {
+                    return BadRequest("You must upload at least one image of the property.");
+                }
+
+                var property = await _propertyService.FindByIdAsync(id);
+                List<byte[]> images = new();
+
+                foreach (var image in propertyImages)
+                {
+                    if (image.Length > 0)
+                    {
+                        using (var stream = new MemoryStream())
+                        {
+                            await image.CopyToAsync(stream);
+                            images.Add(stream.ToArray());
+                        }
+                    }
+                }
+
+                await _propertyImageService.SavePhotos(property, images);
+                return Ok(new { Message = "Images saved successfully" });
+            }
+            catch (NotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+    }
+}
---
+++ RealEstate/RealEstate.Test/Presentation/PropertiesControllerTest.cs
@@ -0,0 +1,29 @@
+﻿using Moq;
+using RealEstate.Application.Accounts;
+using RealEstate.Domain.Interfaces.Services;
+using RealEstate.Presentation.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstate.Test.Presentation
+{
+    [TestFixture]
+    public class PropertiesControllerTest
+    {
+
+        private Mock<IPropertyService> _propertyService;
+        private Mock<IAccountService> _accountService;
+        private PropertiesController _propertiesController;
+
+        [SetUp]
+        public void Setup()
+        {
+            _propertyService = new Mock<IPropertyService>();
+            _accountService = new Mock<IAccountService>();
+            _propertiesController = new PropertiesController(_propertyService.Object, _accountService.Object);
+        }
+    }
+}
